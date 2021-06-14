5e2c5995184d3d3725527ea65f57c449d13102b0
venkatesh6450 <52770723+venkatesh6450@users.noreply.github.com>
2021-06-14 09:42:31 +0530

    commit changes

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -11,8 +11,6 @@ function LandingPage(){
 	            <div class="text">
 	               Book A Ticket A Now and Watch The Movie At Our Screens
 	            </div>
-	            <p>We're providing free content on our platform for future programmers.<br/> 
-	            If you want to become a good programmer then Join our community now & level up your skills with u </p>
 	            <a href="/movies" > <button class="btn">Book a Ticket Now</button></a>
         </div>
     	</div>
